ead16efa55c22ffaf7b51480914ecddfb7d16d4c
1306 <53288823+eve31@users.noreply.github.com>
2022-09-28 11:18:28 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default () => {
   return (
     <AppContainer>
       <SEO
-        title="বাংলা ডেভেলপার চিটশিট"
+        title="Bangla Development Site"
         description="ডেভেলপারদের জন্য, ডেভেলপারদের দ্বারা, মাতৃভাষা বাংলায় সবচেয়ে বড় চিটশিট ভান্ডার"
       />
       <HeroContainer />
